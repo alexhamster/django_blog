32f3d2eb5bbdd4c285bf49ea93536c24ef2eb8dc
Alex <soliaris2012@mail.ru>
2020-06-12 15:45:51 +0500

    added index.html with bootstrap

--- board/models.py
+++ board/models.py
@@ -1,3 +1,17 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    header = models.CharField(verbose_name='header', max_length=100, null=True)
+    body = models.TextField(verbose_name='text_of_post', null=False, default='Empty post...')
+    pub_date = models.DateTimeField(verbose_name='date published')
+    views_count = models.IntegerField(verbose_name='views count', default=0)
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - \
+               datetime.timedelta(days=7)
+
+    def __str__(self):
+        return self.header
